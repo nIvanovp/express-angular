6ab35574d3a66cde2e58097cc31088e6ba90bee2
nikolai <n.ivanov.p@gmail.com>
2016-10-14 11:56:32 +0300

    test-trigger

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var morgan = require('morgan'); 		// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-
 app.use(express.static(__dirname + '/public')); 				// set static path
 app.use(morgan('dev')); 										// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
